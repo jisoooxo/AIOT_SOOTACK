5881baa0e7955a2412015799b039ffddcf15a93c
leejunmi <jm05102041@gmail.com>
2026-09-15 19:06:28 +0900

    add vision node

--- src/aiot_control_pkg/setup.py
+++ src/aiot_control_pkg/setup.py
@@ -56,6 +56,7 @@ setup(
             'motor_control_node = aiot_control_pkg.motor_control_node:main',
             'motor_compliance_node = aiot_control_pkg.motor_compliance_node:main',
             'aiot_compliance_node = aiot_control_pkg.aiot_compliance_node:main',
+            'box_detect_node = aiot_control_pkg.box_detect_node:main',
             
         ],
     },
